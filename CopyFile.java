95134b454e74840877ef603e8a0d7fbd83ee59ba
santeena-varghese <77263875+santeena-varghese@users.noreply.github.com>
2021-10-03 11:49:38 +0530

    Add files via upload

--- CopyFile.java
+++ CopyFile.java
@@ -1,23 +1,28 @@
+/*Aim: Write a program to copy one file to another.
+Java code:
+*/
 import java.io.*;
-
-public class CopyFile
+class CopyFile
 {
-	public static void main(String args[])throws IOException,FileNotFoundException
-	{
-		int i;
-		File f1=new File("D:\\JAVA\\labcycle 2\\qtn12\\file1.txt");
-		File f2=new File("D:\\JAVA\\labcycle 2\\qtn12\\file2.txt");
-		FileInputStream in=new FileInputStream(f1);
-		FileOutputStream op=new FileOutputStream(f2);
-		do 
-		{
-			i=in.read();
-			if(i!=-1)
-			op.write(i);
-		 
-        }
-        while(i!=-1);
-        in.close();
-		op.close();
-	}
+ public static void main(String args[])throws IOException, FileNotFoundException
+  {
+    int i;
+     File f1=new File("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\myname.txt");
+     File f2=new File("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\newone.txt");
+     FileInputStream fin=new FileInputStream(f1);
+     FileOutputStream fout=new FileOutputStream(f2);
+     do
+     {
+      i=fin.read();
+      if (i !=-1)
+        fout.write(i);
+     }while(i!=-1);
+   System.out.println("The entire content of the myname file is copied into the file newone");
+    fin.close();
+     fout.close();
+ }
 }
+/*output
+>java CopyFile
+The entire content of the myname file is copied into the file newone
+*/
--- OddEven.java
+++ OddEven.java
@@ -1,27 +1,44 @@
 import java.io.*;
-
+import java.util.Scanner;
 public class OddEven
-{
-	public static void main(String args[])throws IOException,FileNotFoundException
-	{
-		int i;
-		File f1=new File("D:\\JAVA\\labcycle 2\\qtn13\\oddeven.txt");
-		File f2=new File("D:\\JAVA\\labcycle 2\\qtn13\\even.txt");
-		File f3=new File("D:\\JAVA\\labcycle 2\\qtn13\\odd.txt");
-		FileInputStream in=new FileInputStream(f1);
-		FileOutputStream op1=new FileOutputStream(f2);
-		FileOutputStream op2=new FileOutputStream(f3);
-		do 
-		{
-			i=in.read();
-			if(i%2==0)
-			op1.write(i);
-			else
-			op2.write(i);
+ {
+   public static void main(String args[])
+    { 
+      int a[]=new int[5];
+     try
+      {
+       FileWriter f1=new FileWriter("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\odd.txt");
+       FileWriter f2=new FileWriter("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\even.txt");
+        FileWriter oe = new FileWriter("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\OodEvensfile.txt");
+       Scanner sc=new Scanner(System.in);
+       System.out.println(" enter any 5 no.s");
+       for (int i=0; i<5; i++)
+        {
+          a[i]=sc.nextInt();
+         oe.write(a[i]+"\n");
         }
-        while(i!=-1);
-        in.close();
-		op1.close();
-		op2.close();
-	}
+        oe.close();
+        System.out.println("Successfully wrote to the file:");
+        String s;
+        FileReader fr=new FileReader("C:\\Users\\junaid ansar\\Desktop\\ansar\\java\\file\\OodEvensfile.txt"); 
+        BufferedReader br=new BufferedReader(fr);
+       while((s=br.readLine())!=null)
+        {
+         int c=Integer.parseInt(s);
+          if(c%2 ==0)
+             f2.write(c+"\n");
+          else
+            f1.write(c+"\n");
+            
+         }
+         f1.close();
+         f2.close();
+         fr.close();
+    
+      }
+     catch (IOException e)
+      { 
+        System.out.println("An error occurred."); 
+      }
+   }
 }
